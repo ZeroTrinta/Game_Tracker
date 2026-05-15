b036e43ab27b49353ba3d189e69e3f084db880c0
Enzo Tamietto <152907776+ZeroTrinta@users.noreply.github.com>
2026-05-15 15:54:03 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Troque 'Game_Tracker' pelo nome exato do seu repositório no GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/Game_Tracker/',
+  base: '/',
 })
